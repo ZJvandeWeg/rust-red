acf56f4bad90d908fa072679a16818eb46d81903
Li Wei <oldrev@gmail.com>
2024-09-04 15:17:38 +0800

    remove libloading

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use std::sync::Arc;
 use clap::Parser;
 use tokio_util::sync::CancellationToken;
 
-// use libloading::Library;
-
 use edgelink_core::runtime::engine::FlowEngine;
 use edgelink_core::runtime::registry::{Registry, RegistryBuilder};
 use edgelink_core::*;
@@ -17,44 +15,6 @@ mod consts;
 
 pub use cliargs::*;
 
-/*
-use core::{Plugin, PluginRegistrar};
-
-struct Registrar {
-    plugins: Vec<Box<dyn Plugin>>,
-}
-
-impl PluginRegistrar for Registrar {
-    fn register_plugin(&mut self, plugin: Box<dyn Plugin>) {
-        self.plugins.push(plugin);
-    }
-}
-
-fn main() {
-    let mut registrar = Registrar {
-        plugins: Vec::new(),
-    };
-
-    for path in std::env::args_os().skip(1) {
-        // In this code, we never close the shared library - if you need to be able to unload the
-        // library, that will require more work.
-        let lib = Box::leak(Box::new(Library::new(path).unwrap()));
-        // NOTE: You need to do something to ensure you're only loading "safe" code. Out of scope
-        // for this code.
-        unsafe {
-            let func: libloading::Symbol<unsafe extern "C" fn(&mut dyn PluginRegistrar) -> ()> =
-                lib.get(b"plugin_entry").unwrap();
-            func(&mut registrar);
-        }
-    }
-
-    for plugin in registrar.plugins {
-        plugin.callback1();
-        dbg!(plugin.callback2(7));
-    }
-}
-
-*/
 pub(crate) fn log_init(elargs: &CliArgs) {
     if let Some(ref log_path) = elargs.log_path {
         log4rs::init_file(log_path, Default::default()).unwrap();
