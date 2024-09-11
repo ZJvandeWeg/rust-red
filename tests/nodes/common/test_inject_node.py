ec2f667aca3a11c750ef0c2ffb8043cdeeaea8df
Li Wei <oldrev@gmail.com>
2024-09-12 00:37:00 +0800

    more tests for inject node

--- tests/nodes/common/test_inject_node.py
+++ tests/nodes/common/test_inject_node.py
@@ -44,9 +44,8 @@ class TestInjectNode:
         val_buf = '[1,2,3,4,5]'
         await basic_test("bin", val_buf, json.loads(val_buf))
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''inject value of environment variable''')
+    @pytest.mark.it('inject value of environment variable')
     async def test_0002(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
@@ -61,9 +60,8 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "foo"
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''inject name of node as environment variable''')
+    @pytest.mark.it('inject name of node as environment variable')
     async def test_0003(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
@@ -76,9 +74,8 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "NAME"
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''inject id of node as environment variable''')
+    @pytest.mark.it('inject id of node as environment variable')
     async def test_0004(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
@@ -90,7 +87,6 @@ class TestInjectNode:
         msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "0000000000000001"
-
 
     @pytest.mark.asyncio
     @pytest.mark.it('''inject path of node as environment variable''')
@@ -106,7 +102,6 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "0000000000000100/0000000000000001"
 
-
     @pytest.mark.asyncio
     @pytest.mark.it('''inject name of flow as environment variable''')
     async def test_0006(self):
@@ -121,9 +116,8 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "FLOW"
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''inject id of flow as environment variable''')
+    @pytest.mark.it('inject id of flow as environment variable')
     async def test_0007(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
@@ -136,14 +130,13 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "0000000000000100"
 
-
     @pytest.mark.asyncio
     @pytest.mark.it('''inject name of group as environment variable''')
     async def test_0008(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "name": "NAME", "once": True, "onceDelay": 0.0, "repeat": "",
-            "g": "FF", "topic": "t1", "payload": "NR_GROUP_NAME", "payloadType": "env", "wires": [["2"]]},
+             "g": "FF", "topic": "t1", "payload": "NR_GROUP_NAME", "payloadType": "env", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
             {"id": "FF", "z": "100", "type": "group", "name": "GROUP"}
         ]
@@ -152,14 +145,13 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "GROUP"
 
-
     @pytest.mark.asyncio
     @pytest.mark.it('''inject id of group as environment variable''')
     async def test_0009(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "name": "NAME", "once": True, "onceDelay": 0.0, "repeat": "",
-            "g": "FF", "topic": "t1", "payload": "NR_GROUP_ID", "payloadType": "env", "wires": [["2"]]},
+             "g": "FF", "topic": "t1", "payload": "NR_GROUP_ID", "payloadType": "env", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
             {"id": "FF", "z": "100", "type": "group", "name": "GROUP"}
         ]
@@ -168,14 +160,13 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "00000000000000ff"
 
-
     @pytest.mark.asyncio
     @pytest.mark.it('''inject name of node as environment variable by substitution''')
     async def test_0010(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "name": "NAME", "once": True, "onceDelay": 0.0, "repeat": "",
-            "topic": "t1", "payload": r"${NR_NODE_NAME}", "payloadType": "env", "wires": [["2"]]},
+             "topic": "t1", "payload": r"${NR_NODE_NAME}", "payloadType": "env", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -183,21 +174,95 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "NAME"
 
-
     @pytest.mark.asyncio
     @pytest.mark.it('''inject id of node as environment variable by substitution''')
     async def test_0011(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "0000000000000001", "z": "100", "type": "inject", "name": "NAME",
-            "once": True, "onceDelay": 0.0, "repeat": "",
-            "topic": "t1", "payload": r"${NR_NODE_ID}", "payloadType": "env", "wires": [["2"]]},
+             "once": True, "onceDelay": 0.0, "repeat": "",
+             "topic": "t1", "payload": r"${NR_NODE_ID}", "payloadType": "env", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
         msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] == "0000000000000001"
+
+    @pytest.mark.asyncio
+    @pytest.mark.it('inject path of node as environment variable by substitution')
+    async def test_0012(self):
+        flows = [
+            {"id": "100", "type": "tab"},  # flow 1
+            {"id": "0000000000000001", "z": "100", "type": "inject", "name": "NAME",
+             "once": True, "onceDelay": 0.0, "repeat": "",
+             "topic": "t1", "payload": r"${NR_NODE_PATH}", "payloadType": "env", "wires": [["2"]]},  # CHECKME
+            {"id": "2", "z": "100", "type": "console-json"},
+        ]
+        injections = []
+        msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
+        assert msgs[0]["payload"] == "0000000000000100/0000000000000001"
+
+    @pytest.mark.asyncio
+    @pytest.mark.it('inject name of flow as environment variable by substitution')
+    async def test_0013(self):
+        flows = [
+            {"id": "100", "type": "tab", "label": "FLOW"},  # flow 1
+            {"id": "0000000000000001", "z": "100", "type": "inject", "name": "NAME",
+             "once": True, "onceDelay": 0.0, "repeat": "",
+             # CHECKME
+             "topic": "t1", "payload": r"${NR_FLOW_NAME}", "payloadType": "env",
+             "wires": [["2"]]},
+            {"id": "2", "z": "100", "type": "console-json"},
+        ]
+        injections = []
+        msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
+        assert msgs[0]["payload"] == "FLOW"
+
+    @pytest.mark.asyncio
+    @pytest.mark.it('inject id of flow as environment variable')
+    async def test_0014(self):
+        flows = [
+            {"id": "100", "type": "tab", "label": "FLOW"},  # flow 1
+            {"id": "0000000000000001", "z": "100", "type": "inject", "name": "NAME",
+             "once": True, "onceDelay": 0.0, "repeat": "",
+             # CHECKME
+             "topic": "t1", "payload": r"${NR_FLOW_ID}", "payloadType": "env",
+             "wires": [["2"]]},
+            {"id": "2", "z": "100", "type": "console-json"},
+        ]
+        injections = []
+        msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
+        assert msgs[0]["payload"] == "0000000000000100"
+
+    @pytest.mark.asyncio
+    @pytest.mark.it('inject name of group as environment variable by substitution')
+    async def test_00015(self):
+        flows = [
+            {"id": "100", "type": "tab"},  # flow 1
+            {"id": "1", "z": "100", "g": "1000", "type": "inject", "name": "NAME", "once": True, "onceDelay": 0.0, "repeat": "",
+                "topic": "t1", "payload": "${NR_GROUP_NAME}", "payloadType": "env", "wires": [["2"]]},
+            {"id": "1000", "type": "group", "name": "GROUP", "z": "100"},
+            {"id": "2", "z": "100", "type": "console-json"}
+        ]
+        injections = []
+        msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
+        assert msgs[0]["payload"] == "GROUP"
+
+    @pytest.mark.asyncio
+    @pytest.mark.it('inject id of group as environment variable by substitution')
+    async def test_00016(self):
+        flows = [
+            {"id": "100", "type": "tab"},  # flow 1
+            {"id": "1", "z": "100", "g": "1000", "type": "inject", "name": "NAME", "once": True, "onceDelay": 0.0, "repeat": "",
+                "topic": "t1", "payload": "${NR_GROUP_ID}", "payloadType": "env", "wires": [["2"]]},
+            {"id": "1000", "type": "group", "name": "GROUP", "z": "100"},
+            {"id": "2", "z": "100", "type": "console-json"}
+        ]
+        injections = []
+        msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
+        assert msgs[0]["payload"] == "0000000000001000"
+
 
 
     """
@@ -216,29 +281,27 @@ class TestInjectNode:
         assert msgs[0]["payload"] == "changeMe"
     """
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject once with default delay property''')
+    @pytest.mark.it('should inject once with default delay property')
     async def test_0201(self):
         # Since we cannot got the property in the node
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "once": True,
-            "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
+             "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
         msgs = await run_flow_with_msgs_ntimes(flows, injections, 1)
         assert msgs[0]["topic"] == 't1'
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject once with default delay''')
+    @pytest.mark.it('should inject once with default delay')
     async def test_0202(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "once": True,
-            "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
+             "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -248,14 +311,13 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]['payload'] < expected_time
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject once with 500 msec. delay''')
+    @pytest.mark.it('should inject once with 500 msec. delay')
     async def test_0203(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "once": True, "onceDelay": 0.5,
-            "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
+             "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -265,14 +327,13 @@ class TestInjectNode:
         assert msgs[0]["payload"] >= start_time + 500.0
         assert msgs[0]["payload"] < start_time + 1000.0  # in 1-second
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject once with delay of two seconds''')
+    @pytest.mark.it('should inject once with delay of two seconds')
     async def test_0204(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "once": True, "onceDelay": 2,
-            "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
+             "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -282,14 +343,13 @@ class TestInjectNode:
         assert msgs[0]["payload"] >= start_time + 2000.0
         assert msgs[0]["payload"] < start_time + 2700.0
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject repeatedly''')
+    @pytest.mark.it('should inject repeatedly')
     async def test_0205(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "repeat": 0.2,
-            "topic": "t2", "payload": "payload", "payloadType": "str", "wires": [["2"]]},
+             "topic": "t2", "payload": "payload", "payloadType": "str", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -299,15 +359,14 @@ class TestInjectNode:
         assert msgs[1]["topic"] == 't2'
         assert msgs[1]["payload"] == "payload"
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject once with delay of two seconds and repeatedly''')
+    @pytest.mark.it('should inject once with delay of two seconds and repeatedly')
     async def test_0206(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject",
-            "once": True, "onceDelay": 1.2, "repeat": 0.2,
-            "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
+             "once": True, "onceDelay": 1.2, "repeat": 0.2,
+             "topic": "t1", "payload": "", "payloadType": "date", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -316,14 +375,13 @@ class TestInjectNode:
         assert msgs[0]["topic"] == 't1'
         assert msgs[0]["payload"] > start_time + 1000.0
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject with cron''')
+    @pytest.mark.it('should inject with cron')
     async def test_0207(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
             {"id": "1", "z": "100", "type": "inject", "crontab": "* * * * * *",
-            "topic": "t3", "payload": "", "payloadType": "date", "wires": [["2"]]},
+             "topic": "t3", "payload": "", "payloadType": "date", "wires": [["2"]]},
             {"id": "2", "z": "100", "type": "console-json"},
         ]
         injections = []
@@ -334,9 +392,8 @@ class TestInjectNode:
         assert isinstance(payload, float) or isinstance(payload, int)
         assert payload > start_time
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject multiple properties''')
+    @pytest.mark.it('should inject multiple properties')
     async def test_0208(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
@@ -362,7 +419,6 @@ class TestInjectNode:
         assert msg["payload"] == "foo"
         assert msg["x"] == 10
         # assert msg["y"] == 12
-
 
     """
     # EdgeLink doesn't support the msg injection for `inject` node
@@ -404,9 +460,8 @@ class TestInjectNode:
         # assert msg["y"] == 12
     """
 
-
     @pytest.mark.asyncio
-    @pytest.mark.it('''should inject multiple properties using legacy props if needed''')
+    @pytest.mark.it('should inject multiple properties using legacy props if needed')
     async def test_0210(self):
         flows = [
             {"id": "100", "type": "tab"},  # flow 1
@@ -431,7 +486,6 @@ class TestInjectNode:
         msg = msgs[0]
         assert msg["topic"] == "foo"
         assert msg["payload"] == 123
-
 
     # 0211: should report invalid JSONata expression
     # We don't support JSONata yet...
