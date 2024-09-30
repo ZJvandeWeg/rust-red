1c6bbfeb65583afed9b47a8fc851ce47e188b47c
Li Wei <oldrev@gmail.com>
2024-09-30 12:15:42 +0800

    refactor: split subflow and flow

--- crates/core/src/runtime/flow.rs
+++ crates/core/src/runtime/flow.rs
@@ -11,6 +11,7 @@ use tokio_util::sync::CancellationToken;
 
 use super::context::Context;
 use super::group::{Group, GroupParent};
+use super::subflow::{SubflowOutputPort, SubflowState};
 use crate::runtime::engine::Engine;
 use crate::runtime::env::*;
 use crate::runtime::model::json::*;
@@ -48,22 +49,6 @@ impl Default for FlowArgs {
 }
 
 #[derive(Debug)]
-pub struct SubflowOutputPort {
-    index: usize,
-    owner: Weak<Flow>,
-    msg_tx: MsgSender,
-    msg_rx: MsgReceiverHolder,
-}
-
-#[derive(Debug)]
-pub struct SubflowState {
-    instance_node: Option<Arc<dyn FlowNodeBehavior>>,
-    in_nodes: Vec<Arc<dyn FlowNodeBehavior>>,
-    tx_tasks: JoinSet<()>,
-    tx_ports: Vec<Arc<SubflowOutputPort>>,
-}
-
-#[derive(Debug)]
 pub(crate) struct FlowState {
     pub(crate) groups: DashMap<ElementId, Arc<Group>>,
     pub(crate) nodes: DashMap<ElementId, Arc<dyn FlowNodeBehavior>>,
@@ -92,9 +77,9 @@ pub struct Flow {
     pub engine: Weak<Engine>,
 
     pub stop_token: CancellationToken,
+    pub(crate) subflow_state: Option<std::sync::RwLock<SubflowState>>,
 
     state: FlowState,
-    subflow_state: Option<std::sync::RwLock<SubflowState>>,
     envs: Arc<EnvStore>,
     context: Arc<Context>,
 }
@@ -139,77 +124,6 @@ impl FlowsElement for Flow {
             self.id.to_string()
         }
     }
-}
-
-impl SubflowOutputPort {
-    async fn tx_task(&self, stop_token: CancellationToken) {
-        while !stop_token.is_cancelled() {
-            match self.msg_rx.recv_msg(stop_token.clone()).await {
-                Ok(msg) => {
-                    // Find out the subflow:xxx node
-                    let instance_node = {
-                        let flow =
-                            self.owner.upgrade().expect("The owner of this sub-flow node has been released already!!!");
-
-                        let subflow_state = flow.subflow_state.as_ref().expect("Subflow must have a subflow_state!");
-
-                        let subflow_state_guard =
-                            subflow_state.read().expect("Cannot acquire the lock of field `subflow_state`!!!");
-
-                        subflow_state_guard.instance_node.clone()
-                    };
-
-                    if let Some(instance_node) = instance_node {
-                        let instance_node = instance_node.clone();
-                        let envelope = Envelope { port: self.index, msg };
-                        if let Err(e) = instance_node.fan_out_one(&envelope, stop_token.clone()).await {
-                            log::warn!("Failed to fan-out message: {:?}", e);
-                        }
-                    } else {
-                        log::warn!("The sub-flow does not have a subflow node");
-                    }
-                }
-
-                Err(e) => {
-                    log::error!("Failed to receive msg in subflow_tx_task: {:?}", e);
-                }
-            }
-        }
-    }
-}
-
-impl SubflowState {
-    fn populate_in_nodes(&mut self, flow_state: &FlowState, flow_config: &RedFlowConfig) -> crate::Result<()> {
-        // this is a subflow with in ports
-        for wire_obj in flow_config.in_ports.iter().flat_map(|x| x.wires.iter()) {
-            if let Some(node) = flow_state.nodes.get(&wire_obj.id) {
-                self.in_nodes.push(node.clone());
-            } else {
-                log::warn!("Can not found node(id='{}')", wire_obj.id);
-            }
-        }
-        Ok(())
-    }
-
-    fn start_tx_tasks(&mut self, stop_token: CancellationToken) -> crate::Result<()> {
-        for tx_port in self.tx_ports.iter() {
-            let child_stop_token = stop_token.clone();
-            let port_cloned = tx_port.clone();
-            self.tx_tasks.spawn(async move {
-                port_cloned.tx_task(child_stop_token.clone()).await;
-            });
-        }
-        Ok(())
-    }
-
-    /*
-    async fn stop_tx_tasks(&mut self) -> crate::Result<()> {
-        while self.tx_tasks.join_next().await.is_some() {
-            //
-        }
-        Ok(())
-    }
-    */
 }
 
 impl FlowState {
